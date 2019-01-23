ad01ae74b95c0abde4923de470c9b0e4339f6fa5
Gustaw <gutek488@gmail.com>
2019-01-23 17:34:35 +0100

    Ukończono dzień 1

--- app/zadanie03.js
+++ app/zadanie03.js
@@ -1,1 +1,9 @@
-//Twój kod
+sumArgs();
+
+function sumArgs() {
+    let arr = [];
+    for (let i = 2; i < process.argv.length; i++) {
+        arr.push(Number(process.argv[i]));
+    }
+     console.log(arr.reduce((a,b)=>a+b));
+}
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,11 @@
-//Twój kod
+setTime();
+
+function setTime() {
+    for (let i = 2; i < process.argv.length; i++) {
+        setTimeout(function () {
+            console.log(process.argv[i])
+
+        }, Number(process.argv[i])*1000);
+
+    }
+}
